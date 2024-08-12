bd97281b320f1d8b74899d39ae8b353bc6c82a4b
José Neto <jnan@discente.ifpe.edu.br>
2024-08-12 17:31:46 -0300

    Add User Registration - V7C

--- back_web_chat-sockets/index.js
+++ back_web_chat-sockets/index.js
@@ -2,22 +2,20 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import express from "express";
 import cors from "cors"
+import userController from "./controller/userController.js"
+import connectMongo from "./config/db.js";
 
 const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-    }
-});
+// const server = createServer(app);
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
-io.on("connection", (socket) =>{
-    console.log(socket.id);
-    
-});
+app.use("/user", userController);
 
-server.listen(3333, ()=>{
-    console.log("3333, iai")
-})
+connectMongo().then(() => {
+    app.listen(3333, ()=>{
+        console.log("Server Working");
+    })
+}).catch((error) => {console.log(error);})
+
